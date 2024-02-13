4f964300fca066b71196d6532ce82fbc116409c9
Ridho Fauzi Grafika <ridh21142ti@student.nurulfikri.ac.id>
2024-02-13 10:45:31 +0700

    Meals Application

---
+++ GrandNotedMarkuplanguage/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import { useGlobalContext } from "./Context";
+import Favorites from "./components/Favorites";
+import Meals from "./components/Meals";
+import Modal from "./components/Modal";
+import Search from "./components/Search";
+
+function App() {
+  const { showModal, favorites } = useGlobalContext();
+  return (
+    <>
+      <main>
+        <Search />
+        {favorites.length > 0 && <Favorites />}
+        <Meals />
+        {showModal && <Modal />}
+      </main>
+    </>
+  );
+}
+
+export default App;
